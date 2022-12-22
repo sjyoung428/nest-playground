8d57d7ce3edd55d735d76ccafb55ba3ee557a6dd
young <young@youngui-MacBookAir.local>
2022-12-22 18:04:30 +0900

    feat: create boards controller & implicit private boardsService

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,8 @@
 import { Controller } from '@nestjs/common';
+import { BoardsService } from './boards.service';
 
 @Controller('boards')
-export class BoardsController {}
+export class BoardsController {
+  // 암묵적으로 private boardsService: BoardsService; 를 선언
+  constructor(private boardsService: BoardsService) {}
+}
