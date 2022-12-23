bb66b9831af0d44304e6e32c878421d35ae8fbb6
sjyoung428 <sjy8267@naver.com>
2022-12-23 21:57:48 +0900

    feat: prisma setting (board schema)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BoardsModule } from './boards/boards.module';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [BoardsModule],
+  imports: [BoardsModule, PrismaModule],
+  providers: [PrismaService],
 })
 export class AppModule {}
--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
 
 @Module({
   controllers: [BoardsController],
-  providers: [BoardsService],
+  providers: [BoardsService, PrismaService],
 })
 export class BoardsModule {}
