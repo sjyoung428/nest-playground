18604813f59a798563904a424355015205aaeda6
sjyoung428 <sjy8267@naver.com>
2022-12-28 16:00:20 +0900

    feat: add signUp method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthCredentialDto } from './dto/auth-credential.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
+  async signUp(authCredentialDto: AuthCredentialDto): Promise<void> {
+    const { username, password } = authCredentialDto;
+
+    await this.prisma.user.create({
+      data: {
+        username,
+        password,
+      },
+    });
+  }
+}
