80256d18d4af7b52d13e773c33cf07b2ba47c1eb
sjyoung428 <sjy8267@naver.com>
2022-12-28 15:31:21 +0900

    feat: generate auth module controller service model

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { BoardsModule } from './boards/boards.module';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [BoardsModule, PrismaModule],
+  imports: [BoardsModule, PrismaModule, AuthModule],
   providers: [PrismaService],
 })
 export class AppModule {}
